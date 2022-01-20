b296a33e508ea6c8d462bbb5d6f9a84820b20036
Hammad <hammadyasub14@gmail.com>
2022-01-20 12:59:45 +0530

    add more styles and finish all functionality

--- assets/javaScript/todoScript.js
+++ assets/javaScript/todoScript.js
@@ -1,9 +1,6 @@
 $(document).ready(function(){
-    $('input[type="radio"]').click(function(){
+    $('input[type="checkbox"]').click(function(){
         var inputValue = $(this).attr("value");
-        console.log(inputValue);
-        var link = '/delete-contact/' + inputValue;
-        console.log(link);
         $('#anchor-delete').attr('href',function(){
             return '/delete-contact/?id=' + inputValue;
         });
